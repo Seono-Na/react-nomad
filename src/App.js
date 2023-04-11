6c958e6113caab2957df0f77ea67be0578d4eabc
Seonp_Na <seono5670@gmail.com>
2023-04-11 16:46:48 +0900

    useEffect function

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   const [counter, setValue] = useState(0)
   const onClick = () => setValue((prev) => prev + 1)
   console.log("i run all the time")
-  const iRunOnlyOnce = () => {
-    console.log("i run only once")
-  };
+
   useEffect(() => {
     console.log("CALL THE API...")
   }, [])
