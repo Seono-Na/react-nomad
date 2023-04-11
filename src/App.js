5509d47c601c06c7e9aff9bae987d0727c2062f5
Seonp_Na <seono5670@gmail.com>
2023-04-11 16:57:50 +0900

    studying Deps

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time")
 
   useEffect(() => {
@@ -11,6 +13,7 @@ function App() {
   }, [])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
