5d612183d9cd50d486eff8b00323a885d60b39bd
Seonp_Na <seono5670@gmail.com>
2023-04-12 11:31:20 +0900

    cleanup function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0)
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time")
+function Hello() {
+  const destroydFn = () => {
+    console.log("destroy..")
+  }
+  const Hellofn = () => {
+    console.log("create Hello!")
+    return destroydFn
+  }
+  useEffect(Hellofn, [])
+  return <h1>Hello</h1>
+}
 
-  useEffect(() => {
-    console.log("CALL THE API...")
-  }, [])
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello></Hello> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
