8bd434d3879be8e52a562b60a64ed891311091a4
Seonp_Na <seono5670@gmail.com>
2023-04-14 16:41:53 +0900

    Parameter

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ coverImg, title, title_long, summary, genres }) {
+function Movie({ id, coverImg, title, title_long, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title_long}</Link>
+        <Link to={`/movie/${id}`}>{title_long}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -17,6 +17,7 @@ function Movie({ coverImg, title, title_long, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   title_long: PropTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,7 @@
+import { useParams } from "react-router-dom"
 function Detail() {
+    const x = useParams()
+    console.log(x)
     return <h1>Detail</h1>
 }
 export default Detail
